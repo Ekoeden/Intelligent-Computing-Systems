191fe35108a30dc790290ab36132f1a63baddb03
Ekoeden <117777165+Ekoeden@users.noreply.github.com>
2024-06-02 21:59:36 +0800

    Update lab2.pl

--- lab2.pl
+++ lab2.pl
@@ -1,28 +1,29 @@
-% 2. Опpеделить, является ли один список подсписком дpугого.
+% 2. Определить, является ли один список подсписком другого.
 
 sublist([], _).
 
 sublist([H | Tsub], [H | T]) :-
     sublist(Tsub, T).
 
-sublist([_ | Tsub], [_ | T]) :-
-    sublist(Tsub, T).
+sublist(Sublist, [_ | T]) :-
+    sublist(Sublist, T).
 
 %sublist([2, 3, 4], [1, 2, 3, 4, 5]).
 
 % 10. Упорядочить список методом пузырька.
 
-swap([X, Y | Rest], [Y, X | Rest]) :- 
+swap([X, Y | Rest], [Y, X | Rest]) :-
     X > Y.
 
-swap([Z | Rest1], [Z | Rest2]) :- 
+swap([Z | Rest1], [Z | Rest2]) :-
     swap(Rest1, Rest2).
 
 bubble_sort(List, SortedList) :-
     swap(List, SwappedList),
+    !,
     bubble_sort(SwappedList, SortedList).
 
-bubble_sort(SortedList, SortedList) :- 
+bubble_sort(SortedList, SortedList) :-
     \+ swap(SortedList, _).
 
 % bubble_sort([8, 3, 4, 1, 5, 9, 2, 6, 5], SortedList).
